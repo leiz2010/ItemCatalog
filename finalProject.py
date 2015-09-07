27a3766e4b796da9ae29b10ea7bb6e407c6ed9ee
Lei Zhang <leiz2010@gmail.com>
2015-09-07 11:51:12 -0700

    tested routes

--- finalProject.py
+++ finalProject.py
@@ -1,16 +1,12 @@
-from flask import flask
+from flask import Flask
 app = Flask(__name__)
-
-if __name__ == '__main__':
-    app.debug = True
-    app.run(host = '0.0.0.0', port = 5000)
 
 @app.route('/')
 @app.route('/restaurants')
 def restaurants():
     return "all restaurants pagae"
 
-@app.rout('/restaurant/new')
+@app.route('/restaurant/new')
 def newRestaurant():
     return "create a new restaurant"
 
@@ -18,22 +14,28 @@ def newRestaurant():
 def editRestaurant(restaurant_id):
     return "edit restaurant"
 
-@app.route('restaurant/<int:restaurant_id>/delete')
+@app.route('/restaurant/<int:restaurant_id>/delete')
 def deletRestaurant(restaurant_id):
     return "delte restaurant"
 
-@app.route('restaurant/<int:restaurant_id>/menu')
+@app.route('/restaurant/<int:restaurant_id>/menu')
 def menu(restaurant_id):
     return "restaurant menu"
 
-@app.route('restaurant/<int:restaurant_id>/menu/new')
+@app.route('/restaurant/<int:restaurant_id>/menu/new')
 def newMenuItem(restaurant_id):
     return "add menu item"
 
-@app.route('restaurant/<int:restaurant_id>/<int:menu_id>/edit')
+@app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/edit')
 def editMenuItem(restaurant_id, menu_id):
     return "edit menu item"
 
-@app.route('restaurant/<int:restaurant_id>/<int:menu_id>/delete')
+@app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/delete')
 def deleteMenuItem(restaurant_id, menu_id):
     return "delete menu item"
+
+
+if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
