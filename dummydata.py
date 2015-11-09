a9a69f8123d7b9bea1cedce8d08ed1e64078d651
Lei Zhang <leiz2010@gmail.com>
2015-11-08 22:02:26 -0800

    updated dummydata.py

--- dummydata.py
+++ dummydata.py
@@ -9,17 +9,9 @@ engine = create_engine('sqlite:///menuwithuser.db')
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
-# A DBSession() instance establishes all conversations with the database
-# and represents a "staging zone" for all the objects loaded into the
-# database session object. Any change made against the objects in the
-# session won't be persisted into the database until you call
-# session.commit(). If you're not happy about the changes, you can
-# revert all of them back to the last commit by calling
-# session.rollback()
 session = DBSession()
 
-
-# Menu for UrbanBurger
+# Menu for Yatta Teriyaki
 type1 = ItemType(category="Appetizer")
 type2 = ItemType(category="Entree")
 type3 = ItemType(category="Dessert")
@@ -28,8 +20,33 @@ session.add(type1)
 session.add(type2)
 session.add(type3)
 session.add(type4)
-#session.commit()
-item = MenuItem(name="Beef Teriyaki", description="Beef wiht rice.", price="8.99", category="Entree")
+
+item1 = MenuItem(
+                name="Beef Teriyaki",
+                description="Beef wiht rice.",
+                price="8.99", category="Entree")
+
+item2 = MenuItem(
+                name="Light Beer",
+                description="Light beer, serving size: 1 glass.",
+                price="2.99", category="Beverage")
+
+item3 = MenuItem(
+                name="Deep fried Tofu",
+                description=(
+                    "Tofu with stuffing deep fried in peanut oil," +
+                    "serving size: 1 glass."),
+                price="4.99", category="Appetizer")
+
+item4 = MenuItem(
+                name="Green Tea Ice Cream",
+                description="Green tea flavor ice cream.",
+                price="2.99", category="Dessert")
+
+session.add(item1)
+session.add(item2)
+session.add(item3)
+session.add(item4)
 session.commit()
 
 print "added menu items!"
